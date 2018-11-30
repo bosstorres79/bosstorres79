e1d4e66d833faec9c1fc5c2d3ecd4e1c37599894
bosstorres79 <44692207+bosstorres79@users.noreply.github.com>
2018-11-30 19:14:55 +0700

    bbs

---
+++ Test23/Newweb/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContentComponent } from './shared/content/content.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { CreateProductComponent } from './home/create-product/create-product.component';
+import { UpdateproductComponent } from './home/updateproduct/updateproduct.component';
+import { BmiProductComponent } from './home/bmi-product/bmi-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContentComponent,
+    HomeComponent,
+    CreateProductComponent,
+    UpdateproductComponent,
+    BmiProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
